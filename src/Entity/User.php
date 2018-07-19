fbe7ab07175c5cc257a1caf0e21cc67939dc50b3
jenaye <stahlilistenvoid@gmail.com>
2018-07-19 22:52:37 +0200

    start add login

---
+++ src/Entity/User.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Houziaux mike : jenaye
+ * Email : mike@les-tilleuls.coop
+ * Date: 19/07/18
+ * Time: 21:00
+ */
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use ApiPlatform\Core\Annotation\ApiResource;
+
+/**
+* @ApiResource()
+* @ORM\Table(name="app_users")
+* @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+*/
+class User implements UserInterface, \Serializable
+{
+
+    /**
+    * @ORM\Column(type="integer")
+    * @ORM\Id
+    * @ORM\GeneratedValue(strategy="AUTO")
+    */
+    private $id;
+
+    /**
+    * @ORM\Column(type="string", length=25, unique=true)
+    */
+    private $username;
+
+
+
+    /**
+    * @ORM\Column(type="string", length=64)
+    */
+    private $password;
+
+
+
+    /**
+    * @ORM\Column(name="is_active", type="boolean")
+    */
+    private $isActive;
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getisActive()
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * @param mixed $isActive
+     */
+    public function setIsActive($isActive): void
+    {
+        $this->isActive = $isActive;
+    }
+
+
+    public function getUsername()
+    {
+    return $this->username;
+    }
+
+    public function getSalt()
+    {
+    return null;
+    }
+
+    public function getPassword()
+    {
+    return $this->password;
+    }
+
+    public function getRoles()
+    {
+    return array('ROLE_USER');
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    /** @see \Serializable::serialize() */
+    public function serialize()
+    {
+        return serialize(array(
+        $this->id,
+        $this->username,
+        $this->password,
+    ));
+    }
+
+/** @see \Serializable::unserialize() */
+    public function unserialize($serialized) {
+    list ($this->id,$this->username,$this->password) = unserialize($serialized, ['allowed_classes' => false]);
+    }
+}
---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jenaye
+ * Date: 19/07/2018
+ * Time: 21:12
+ */
+
+namespace App\Repository;
+
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+use Doctrine\ORM\EntityRepository;
+
+class UserRepository extends EntityRepository implements UserLoaderInterface
+{
+    public function loadUserByUsername($username)
+    {
+        return $this->createQueryBuilder('u')
+            ->where('u.username = :username OR u.email = :email')
+            ->setParameter('username', $username)
+            ->setParameter('email', $username)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+}
