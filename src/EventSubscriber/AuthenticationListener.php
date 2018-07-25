ddc9c476a04c3f8373caa3f0367bade9674f9589
jenaye <stahlilistenvoid@gmail.com>
2018-07-25 13:45:40 +0200

    trying as controller

--- src/EventSubscriber/AuthenticationListener.php
+++ src/EventSubscriber/AuthenticationListener.php
@@ -4,67 +4,29 @@
 namespace App\EventSubscriber;
 
 use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\AuthenticationEvents;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\Security\Core\Event\AuthenticationEvent;
-use Symfony\Component\Security\Core\Event\AuthenticationFailureEvent;
-use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
-use Symfony\Component\HttpFoundation\Cookie;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Doctrine\ORM\EntityManager;
 
-class AuthenticationListener implements EventSubscriberInterface
+class AuthenticationListener extends Controller
 {
-
-
-    protected $container;
-
-    public function __construct(ContainerInterface $container) // this is @service_container
-    {
-        $this->container = $container;
-    }
-
     /**
-     * getSubscribedEvents
-     *
-     * @author 	Houziaux mike <jenaye@protonmail.com>
-     * @return 	array
+     * @param JWTCreatedEvent $event
      */
-    public static function getSubscribedEvents()
+    public function onJWTCreated(JWTCreatedEvent $event)
     {
-        return array(
-            'kernel.request' => 'onKernelRequest',
-            AuthenticationEvents::AUTHENTICATION_FAILURE => 'onAuthenticationFailure',
-            AuthenticationEvents::AUTHENTICATION_SUCCESS => 'onAuthenticationSuccess',
-        );
-    }
-
-
-    public function onAuthenticationFailure()
-    {
-      // nothing
-    }
-
-    // update field lastLogin
-    public function onAuthenticationSuccess()
-    {
-
-        /*
-       $em = $this->container->get('doctrine.orm.entity_manager');
-       $users = $em->getRepository(User::class)->find($id);
-       var_dump('User =>',$users);
-        */
-
+        $user = $event->getUser();
+        $id = $user->getId();
+        $user->getUsername();
+        var_dump($id);
+        exit;
+        $em = $this->container->get('doctrine.orm.entity_manager');
+        $demand = $em->getRepository(User::class)->find($id);
+        $now = (new DateTime('now'))->format('Y-m-d H:i:s');
+        $demand->setL($now);
+        $em->persist($demand);
+        $em->flush();
 
     }
 
-    public function onKernelRequest(GetResponseEvent $event)
-    {
-        // trying to get ID of user, than use it to update field lastLogin
-        $request  = $event->getRequest();
-        var_dump("Parameters => ",$request->request->all());
-    }
 }
